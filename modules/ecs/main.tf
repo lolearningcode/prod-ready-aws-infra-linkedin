5cd25a67ec284b5902777eb3967a957a3b6a542a
Cleo Howard <lolearningcode@gmail.com>
2025-09-28 07:28:45 -0700

    added task definition updates

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -8,6 +8,7 @@ resource "aws_ecs_task_definition" "app" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([
     {
@@ -43,4 +44,24 @@ resource "aws_ecs_service" "app" {
     container_name   = "app"
     container_port   = 80
   }
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "${var.name}-ecs-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
